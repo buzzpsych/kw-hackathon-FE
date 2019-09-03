e0306a90249121fcf32f0eed24ad0e4be750f387
Evan <evan@buzzpsych.com>
2019-09-03 17:26:27 -0400

    task: add username

--- src/graphql/addProduct/index.js
+++ src/graphql/addProduct/index.js
@@ -1,8 +1,15 @@
 import { gql } from "apollo-boost";
 
 export const ADD_PRODUCT = gql`
-  mutation createProduct($name: String!, $description: String!) {
-    createProduct(input: { name: $name, description: $description }) {
+  mutation createProduct(
+    $name: String!
+    $description: String!
+    $username: String!
+  ) {
+    createProduct(
+      input: { name: $name, description: $description, username: $username }
+    ) {
+      username
       name
       description
     }
