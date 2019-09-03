075656374a1b4a5ae4f78e3d897db1b62157dcf1
Carlos Bartolo <carls940@gmail.com>
2019-09-03 14:16:45 -0500

    feat: form component

--- src/views/productReview/index.js
+++ src/views/productReview/index.js
@@ -1,15 +1,9 @@
 import React from "react";
-import {
-  Button,
-  Container,
-  Grid,
-  Header,
-  Input,
-  Label
-} from "semantic-ui-react";
+import { Container, Grid, Header } from "semantic-ui-react";
+import Form from "./components/Form";
 import "./styles.scss";
 
-const productReview = () => {
+const productReview = props => {
   return (
     <Container className="product_review">
       <Grid.Column width={12}>
@@ -17,26 +11,7 @@ const productReview = () => {
           Product #1
         </Header>
       </Grid.Column>
-      <Grid columns={16} className="box">
-        <Grid.Row>
-          <Grid.Column width={10} className="box__description">
-            <Container>
-              <Label>Description:</Label>
-              <Input placeholder="Enter your description" />
-            </Container>
-            <Container>
-              <Label className="label__strong">Review: 3/5</Label>
-            </Container>
-          </Grid.Column>
-          <Grid.Column width={12}>
-            <Container className="box__review">
-              <Label>Enter review:</Label>
-              <Input className="inputText inputText--small" />
-              <Button primary>Submit</Button>
-            </Container>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+      <Form {...props} />
     </Container>
   );
 };
