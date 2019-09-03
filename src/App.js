9912cab16cb9b33f2feb97505fc469a70d3a7f1e
Victor Nieves <victor.nieves@kw.com>
2019-09-03 11:45:05 -0500

    feat: added eslint config

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
 
-const Login = () =>{
-  return <div>Login view is coming....</div>
-}
+const Login = () => {
+  return <div>Login view is coming....</div>;
+};
 const App = () => {
   return (
-    <div className="App">
-      <Router>
-        <div>
-          <Route exact path="/" component={Login} />
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={Login} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
