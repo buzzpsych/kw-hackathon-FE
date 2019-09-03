568bea7a5ebd2f828cb4b5cc748596b9610e32e2
Carlos Bartolo <carls940@gmail.com>
2019-09-03 15:06:31 -0500

    feat: username

--- src/views/productReview/index.js
+++ src/views/productReview/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Grid, Header } from "semantic-ui-react";
 import Form from "./components/Form";
+import { getUsername } from "../../utils";
 import "./styles.scss";
-
 const productReview = props => {
   return (
     <Container className="product_review">
@@ -10,6 +10,7 @@ const productReview = props => {
         <Header as="h2" className="title">
           Product #1
         </Header>
+        <div className="user">Hello, {getUsername()}</div>
       </Grid.Column>
       <Form {...props} />
     </Container>
