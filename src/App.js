5aeff6275d35b258233ac5c30713df5b9813d264
Carlos Bartolo <carls940@gmail.com>
2019-09-03 11:57:13 -0500

    feat: view productReview

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
-
-const Login = () =>{
+import "./App.css";
+import productReview from "./views/productReview";
+/* const Login = () =>{
   return <div>Login view is coming....</div>
-}
+} */
 const App = () => {
   return (
     <div className="App">
       <Router>
         <div>
-          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={productReview} />
         </div>
       </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/views/productReview/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./style.css";
+
+const productReview = () => {
+  return <div>view productReview....</div>;
+};
+
+export default productReview;
