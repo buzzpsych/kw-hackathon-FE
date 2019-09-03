76d5846875db9d4ae96631782e603e1dc64eadff
Carlos Bartolo <carls940@gmail.com>
2019-09-03 13:20:16 -0500

    feat: adding some styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./views/Home";
+import productReview from "./views/productReview";
 
 const App = () => {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={productReview} />
       </div>
     </Router>
   );
--- src/views/productReview/index.js
+++ src/views/productReview/index.js
@@ -1,8 +1,44 @@
 import React from "react";
-// import "./style.css";
+import {
+  Button,
+  Container,
+  Grid,
+  Header,
+  Input,
+  Label
+} from "semantic-ui-react";
+import "./styles.scss";
 
 const productReview = () => {
-  return <div>view productReview....</div>;
+  return (
+    <Container className="product_review">
+      <Grid.Column width={12}>
+        <Header as="h2" className="title">
+          Product #1
+        </Header>
+      </Grid.Column>
+      <Grid columns={16} className="box">
+        <Grid.Row>
+          <Grid.Column width={10} className="box__description">
+            <Container>
+              <Label>Description:</Label>
+              <Input placeholder="Enter your description" />
+            </Container>
+            <Container>
+              <Label className="label__strong">Review: 3/5</Label>
+            </Container>
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <Container className="box__review">
+              <Label>Enter review:</Label>
+              <Input className="inputText inputText--small" />
+              <Button primary>Submit</Button>
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
 };
 
 export default productReview;
