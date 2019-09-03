a55b3c4d6ecdf67355a7be0f38097059eb056796
Evan <evan@buzzpsych.com>
2019-09-03 16:08:06 -0400

    task: skeleton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./views/Home";
 import productReview from "./views/productReview";
+import AddProduct from "./views/AddProduct";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={productReview} />
+        <Route exact path="/products/add" component={AddProduct} />
       </div>
     </Router>
   );
